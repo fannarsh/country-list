2dc9acc87886194a5fac57eec7e4c8e3f9957192
Fannar Snær <fannar@lambagras.is>
2016-10-15 02:37:55 +0000

    Linting with JavaScript Standard Style

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-country-list
-============
+# country-list
 
 Maps ISO 3166-1-alpha-2 codes to English country names and vice versa.
 
 Uses data from http://data.okfn.org/data/country-list
 
-# example
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
+
+## Example
 
 ``` js
 var countries = require('country-list')();
@@ -14,45 +15,45 @@ console.log(countries.getName('IS')); // Iceland
 console.log(countries.getCode('Iceland')); // IS
 ```
 
-# methods
+## Methods
 
 Usage:
 
-```
+``` js
 var countries = require('country-list')();
 ```
 All input is case-insensitive.
 
-## getName(code)
+### getName(code)
 
 Expects a two-digit country code.
 Returns the name for that country.
 If not found, it returns `undefined`.
 
-## getCode(name)
+### getCode(name)
 
 Expects the English country name.
 Returns the code for that country.
 If not found, it returns `undefined`.
 
-## getNames()
+### getNames()
 
 Returns an array of all country names.
 
-## getCodes()
+### getCodes()
 
 Returns an array of all country codes.
 
-## getData()
+### getData()
 
 Returns an array of all country information, in the same format as it gets imported.
 
-# install
+## Install
 
 ``` cli
 npm install country-list
 ```
 
-# license
+## License
 
 MIT
--- country-list.js
+++ country-list.js
@@ -1,42 +1,42 @@
 'use strict'
 
-var data = require('./data.json');
+var data = require('./data.json')
 
 /**
  * Precompute name and code lookups.
  */
-var nameMap = {};
-var codeMap = {};
-data.forEach(function(country) {
-  nameMap[country.name.toLowerCase()] = country.code;
-  codeMap[country.code.toLowerCase()] = country.name;
-});
+var nameMap = {}
+var codeMap = {}
+data.forEach(function (country) {
+  nameMap[country.name.toLowerCase()] = country.code
+  codeMap[country.code.toLowerCase()] = country.name
+})
 
-module.exports = CountryList;
-function CountryList() {
-  if (!(this instanceof CountryList)) return new CountryList();
+module.exports = CountryList
+function CountryList () {
+  if (!(this instanceof CountryList)) return new CountryList()
 };
 
-CountryList.prototype.getCode = function getCode(name) {
-  return nameMap[name.toLowerCase()];
-};
+CountryList.prototype.getCode = function getCode (name) {
+  return nameMap[name.toLowerCase()]
+}
 
-CountryList.prototype.getName = function getName(code) {
-  return codeMap[code.toLowerCase()];
-};
+CountryList.prototype.getName = function getName (code) {
+  return codeMap[code.toLowerCase()]
+}
 
-CountryList.prototype.getNames = function getNames() {
-  return data.map(function(country) {
-    return country.name;
-  });
-};
+CountryList.prototype.getNames = function getNames () {
+  return data.map(function (country) {
+    return country.name
+  })
+}
 
-CountryList.prototype.getCodes = function getCodes() {
-  return data.map(function(country) {
-    return country.code;
-  });
-};
+CountryList.prototype.getCodes = function getCodes () {
+  return data.map(function (country) {
+    return country.code
+  })
+}
 
-CountryList.prototype.getData = function getData() {
-  return data;
-};
+CountryList.prototype.getData = function getData () {
+  return data
+}

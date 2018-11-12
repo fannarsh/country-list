6b97ff26c6f6ceed48e0c6b8aefb59151428fee3
Fannar Snær <fannar@lambagras.is>
2018-11-12 00:50:57 +0000

    Adding Travis CI

--- test/import.js
+++ test/import.js
@@ -3,7 +3,7 @@
 const { test } = require('tap')
 const { getCode, getName } = require('../')
 
-test('get code from country name', t => {
+test('get code and name for country', t => {
   t.equal(getCode('Iceland'), 'IS', 'name "Iceland" should return IS')
   t.equal(getName('IS'), 'Iceland', 'code "IS" should return Iceland')
   t.end()

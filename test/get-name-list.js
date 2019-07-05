e1753c5578b875381bea895913806341de1fc433
Bogdan Chadkin <trysound@yandex.ru>
2019-07-05 17:42:09 +0300

    Tidy up exports with destructuring

--- test/get-name-list.js
+++ test/get-name-list.js
@@ -1,11 +1,11 @@
 'use strict'
 
-var test = require('tap').test
-var countries = require('../')
+var { test } = require('tap')
+var { getNameList } = require('../')
 var data = require('../data.json')
 
 test('get all country names with code as key', function (t) {
-  var list = countries.getNameList()
+  var list = getNameList()
 
   t.type(list, Object, 'country name list is an object')
   t.equal(Object.keys(list).length, data.length, 'country name list should be as many as countries')
